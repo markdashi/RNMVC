f7b603f73936d9a954cc66303746ba4e7b6c5b82
mark6 <wangjunfei@uml-tech.com>
2017-11-28 18:07:05 +0800

    no message

--- XMGCourse/App/Main/CommonFlatView.js
+++ XMGCourse/App/Main/CommonFlatView.js
@@ -13,8 +13,7 @@ import {
 
 import {PropTypes} from 'prop-types'
 
-import CommonGroupItem from 'CommonGroupItem'
-import CommonRowItem from 'CommonRowItem'
+
 
 /**
  *  [s1,s2]
@@ -27,44 +26,27 @@ import CommonRowItem from 'CommonRowItem'
 export default class CommonFlatView extends Component<{}> {
 
 
-    _setupGroup0(groups){
-
-        var item0 = new CommonRowItem('icon_mine_myAccount_address','小码哥','年薪100W');
-        var item1 = new CommonRowItem('icon_mine_myAccount_livingCity','到过的公园','');
-        var item2 = new CommonRowItem('icon_mine_myAccount_password','阅历','');
-
-
-        var group = CommonGroupItem([item0,item1,item2],0);
-        groups.push(group)
+    static propTypes = {
+        groups:PropTypes.array
     }
-
-    _setupGroup1(groups){
-
-        var item0 = new CommonRowItem('icon_mine_myAccount_address','小码哥','年薪100W');
-        var item1 = new CommonRowItem('icon_mine_myAccount_livingCity','到过的公园','');
-        var item2 = new CommonRowItem('icon_mine_myAccount_password','阅历','');
-
-
-        var group = CommonGroupItem([item0,item1],0);
-        groups.push(group)
-    }
-
 
     // 构造
       constructor(props) {
           super(props);
           // 初始状态
 
-          var groups = [];
 
-          //加载第一组数据
-          this._setupGroup0(groups)
-          //加载第二组数据
-          this._setupGroup1(groups)
+          var sectionData = [];
 
+          var groups = this.props.groups;
+
+          groups.forEach((groupItem,index)=>{
+              sectionData.push(groupItem.rowData)
+          })
           this.state = {
-              ds:[]
+              ds:sectionData
           };
+
       }
 
 
@@ -75,8 +57,7 @@ export default class CommonFlatView extends Component<{}> {
     componentDidMount() {
 
 
-
-
+        console.log(this.props.groups);
 
         var dataS = []
 
@@ -93,21 +74,21 @@ export default class CommonFlatView extends Component<{}> {
                 {text: 'SSection2 item0', key: '1'}
             ]
         })
-        this.setState({
-            ds:dataS
-        })
+        // this.setState({
+        //     ds:dataS
+        // })
 
 
     }
 
     _renderSectionHeader({section}){
-        console.log(section)
+        // console.log(section)
        return(
            <Text>{section.s1}</Text>
        )
     }
     _renderItem({item}){
-        console.log(item);
+        // console.log(item);
         return(
             <Text>{item.text}</Text>
         )
--- XMGCourse/App/Main/Main.js
+++ XMGCourse/App/Main/Main.js
@@ -11,12 +11,56 @@ import {
 } from 'react-native';
 
 
+import CommonGroupItem from 'CommonGroupItem'
+import CommonRowItem from 'CommonRowItem'
+
 import CommonFlatView from 'CommonFlatView'
 
 export default class App extends Component<{}> {
+
+    _setupGroup0(groups){
+
+        var item0 = new CommonRowItem('icon_mine_myAccount_address','小码哥','年薪100W');
+        var item1 = new CommonRowItem('icon_mine_myAccount_livingCity','到过的公园','');
+        var item2 = new CommonRowItem('icon_mine_myAccount_password','阅历','');
+
+
+        var group = CommonGroupItem([item0,item1,item2],0);
+        groups.push(group)
+    }
+
+    _setupGroup1(groups){
+
+        var item0 = new CommonRowItem('icon_mine_myAccount_address','小码哥','年薪100W');
+        var item1 = new CommonRowItem('icon_mine_myAccount_livingCity','到过的公园','');
+        var item2 = new CommonRowItem('icon_mine_myAccount_password','阅历','');
+        
+        var group = CommonGroupItem([item0,item1],20);
+        groups.push(group)
+    }
+    
+    // 构造
+      constructor(props) {
+        super(props);
+        // 初始状态
+          
+          var groups = [];
+
+          //加载第一组数据
+          this._setupGroup0(groups)
+          //加载第二组数据
+          this._setupGroup1(groups)
+
+          this.state = {
+              groups:groups
+          };
+          console.log(groups)
+      }
+    
+    
     render() {
         return (
-            <CommonFlatView />
+            <CommonFlatView groups={this.state.groups}/>
         );
     }
 }
